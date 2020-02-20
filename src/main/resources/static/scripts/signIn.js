ff0f66b25175cd65996c11d2fab39eab71211f64
Alex Lim <syl002@uark.edu>
2020-02-20 17:47:17 -0600

    Client Side Functionality Sign In, incomplete

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -3,6 +3,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 function validateForm() {
-	// TODO: Validate the user input
+	
+	var empIdIn = document.getElementById("empId").value;
+	var pswIn = document.getElementById("psw").value;
+	
+	alert("employee id " + empIdIn);
+	alert("password " + pswIn);
+			
+	if (empIdIn.length == 0)	{
+		alert("Employee ID should not be blank");
+	}
+	
+	if (typeof empIdIn == 'number')	{
+		alert("Employee ID should be numeric");
+	}
+	
+	if (pswIn.length == 0)	{
+		alert("The password should not be blank");
+	}
+	
+	else	{
+		alert("Correct");
+	}
+	
 	return true;
 }
