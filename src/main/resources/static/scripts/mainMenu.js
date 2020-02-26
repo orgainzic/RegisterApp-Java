32b1005866fd52f0cc9ff4a87d89906e184b920f
Tyler Stoll <tdstoll@uark.edu>
2020-02-26 09:08:28 -0600

    Update 2: 2-6/7 2 TODO comments. Starting 2-8

---
+++ src/main/resources/static/scripts/mainMenu.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const viewStartTransactionElement = getStartTransactionElement();
+	const viewProductsElement = getViewProductsElement();
+	const viewCreateEmployeeElement = getCreateEmployeeElement();
+	const viewSalesReportElement = getSalesReportElement();
+	const viewCashierReportElement = getCashierReportElement();
+	const elevatedOptions = toggleButtonsElement();
+	
+	var displaySetting = elevatedOptions.style.display;
+	if(displaySetting == "block") {
+		elevatedOptions.style.desplay = "none";
+	}
+	
+	viewStartTransactionElement().addEventListener("click", displayError(errorMessage);
+	viewProductsElement().addEventListener("click", productsElementActionClickHandler);
+	viewCreateEmployeeElement().addEventListener("click", createEmployeeElementActionClickHandler);
+	viewSalesReportElement().addEventListener("click", displayError(errorMessage));
+	viewCashierReportElement().addEventListener("click", displayError(errorMessage));
+
+});	
+
+// TODO:: Adding proper routing tonight during 2-8
+function signOutActionClickHandler() {
+	ajaxDelete("/api/signOut", (callbackResponse) => {
+		if ((callbackResponse.data != null)
+			&& (callbackResponse.data.redirectUrl != null)
+			&& (callbackResponse.data.redirectUrl !== "")) {
+	
+			window.location.replace(callbackResponse.data.redirectUrl);
+		} else {
+			window.location.replace("/");
+		}
+	});
+	
+function signOutActionClickHandler() {
+	ajaxDelete("/api/signOut", (callbackResponse) => {
+		if ((callbackResponse.data != null)
+			&& (callbackResponse.data.redirectUrl != null)
+			&& (callbackResponse.data.redirectUrl !== "")) {
+	
+			window.location.replace(callbackResponse.data.redirectUrl);
+		} else {
+			window.location.replace("/");
+		}
+	});
+	
+function toggleButtonsElement() {
+	return document.getElementById("elevatedAccess";
+}
+function getStartTransactionElement() {
+	return document.getElementById("StartTransaction");
+}
+function getViewProductsElement() {
+	return document.getElementById("ViewProducts");
+}
+function getCreateEmployeeElement() {
+	return document.getElementById("CreateEmployee");
+}
+function getSalesReportElement() {
+	return document.getElementById("SalesReport");
+}
+function getCashierReportElement() {
+	return document.getElementById("CashierReport");
+}
