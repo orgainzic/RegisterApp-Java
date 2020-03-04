58a8c3f165db467656a3d44ec8e4568936635671
TheFlyIsASpy <nicolassherid@gmail.com>
2020-03-04 15:25:59 -0600

    First attempt at server side routing for signin

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -3,7 +3,6 @@ package edu.uark.registerapp.controllers;
 import javax.servlet.http.HttpServletRequest;
 
 import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -14,33 +13,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery;
+import edu.uark.registerapp.commands.employees.EmployeeSignInCommand;
+import edu.uark.registerapp.commands.exceptions.ConflictException;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.EmployeeSignIn;
 
 @Controller
-@RequestMapping(value = "/")
 public class SignInRouteController extends BaseRouteController {
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET, value = "/")
 	public ModelAndView start(@RequestParam final Map<String, String> queryParameters) {
+		ModelAndView modelAndView = new ModelAndView();
 		try{
 			employeeQuery.execute();
-			ModelAndView modelAndView = new ModelAndView(ViewNames.SIGN_IN.getViewName());
+			modelAndView.setViewName(ViewNames.SIGN_IN.getViewName());
 		}catch(NotFoundException e){
-			ModelAndView modelAndView = new ModelAndView(ViewNames.EMPLOYEE_DETAIL.getViewName());
+			modelAndView.setViewName(ViewNames.EMPLOYEE_DETAIL.getViewName());
 		}
 		return modelAndView;
 	}
 
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	@RequestMapping(value = "/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public ModelAndView performSignIn(
 		EmployeeSignIn employeeSignIn, HttpServletRequest request
 	) {
-		
+		//request.getSession().getId();
 		// TODO: Use the credentials provided in the request body
 		//  and the "id" property of the (HttpServletRequest)request.getSession() variable
 		//  to sign in the user
+		ModelAndView modelAndView = new ModelAndView();
+
+		try{
+			employeeSignInCommand.execute();
+		}catch(Exception e){
+			modelAndView.setViewName(ViewNames.SIGN_IN.getViewName());
+			modelAndView.addObject(ViewModelNames.ERROR_MESSAGE.getValue(), e.getMessage());
+			return modelAndView;
+		}
 
 		return new ModelAndView(
 			REDIRECT_PREPEND.concat(
@@ -49,4 +61,5 @@ public class SignInRouteController extends BaseRouteController {
 
 	@Autowired
 	private ActiveEmployeeExistsQuery employeeQuery;
+	private EmployeeSignInCommand employeeSignInCommand;
 }
