40fd6a237f38f481cb2ec7ddf8727893dfc841c6
Tyler Stoll <tdstoll@uark.edu>
2020-03-05 07:29:46 -0600

    Controller file

--- src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
@@ -1,55 +1,50 @@
 package edu.uark.registerapp.controllers;
 
-import java.util.UUID;
+import java.util.Map;
+import java.util.Optional;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import edu.uark.registerapp.commands.products.ProductQuery;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
-import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
 
 @Controller
-@RequestMapping(value = "/productDetail")
-public class ProductDetailRouteController {
+@RequestMapping(value = "/mainMenu")
+public class MainMenuRouteController extends BaseRouteController {
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView start() {
-		return (new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName()))
-			.addObject(
-				ViewModelNames.PRODUCT.getValue(),
-				(new Product()).setLookupCode(StringUtils.EMPTY).setCount(0));
-	}
+	public ModelAndView start(
+		@RequestParam final Map<String, String> queryParameters,
+		final HttpServletRequest request
+	) {
 
-	@RequestMapping(value = "/{productId}", method = RequestMethod.GET)
-	public ModelAndView startWithProduct(@PathVariable final UUID productId) {
-		final ModelAndView modelAndView =
-			new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName());
-
-		try {
-			modelAndView.addObject(
-				ViewModelNames.PRODUCT.getValue(),
-				this.productQuery.setProductId(productId).execute());
-		} catch (final Exception e) {
-			modelAndView.addObject(
-				ViewModelNames.ERROR_MESSAGE.getValue(),
-				e.getMessage());
-			modelAndView.addObject(
-				ViewModelNames.PRODUCT.getValue(),
-				(new Product())
-					.setCount(0)
-					.setLookupCode(StringUtils.EMPTY));
+		final Optional<ActiveUserEntity> activeUserEntity =
+			this.getCurrentUser(request);
+		if (!activeUserEntity.isPresent()) {
+			return this.buildInvalidSessionResponse();
 		}
+		
+		ModelAndView modelAndView =
+			this.setErrorMessageFromQueryString(
+				new ModelAndView(ViewNames.MAIN_MENU.getViewName()),
+				queryParameters);
 
+		// TODO: Examine the ActiveUser classification if you want this information
+		if (!activeUserEntity.isPresent()){
+			ModelAndView modelAndView =
+			new ModelAndView(ViewNames.SIGN_IN.getViewName());
+		}
+		
+		modelAndView.addObject(
+			ViewModelNames.IS_ELEVATED_USER.getValue(),
+			true);
+		
 		return modelAndView;
 	}
-
-	// Properties
-	@Autowired
-	private ProductQuery productQuery;
 }
--- src/main/resources/static/scripts/mainMenu.js
+++ src/main/resources/static/scripts/mainMenu.js
@@ -1,27 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const viewStartTransactionElement = getStartTransactionElement();
-	const viewProductsElement = getViewProductsElement();
+	const viewProductsElement = getProductsElement();
 	const viewCreateEmployeeElement = getCreateEmployeeElement();
 	const viewSalesReportElement = getSalesReportElement();
 	const viewCashierReportElement = getCashierReportElement();
-	const elevatedOptions = toggleButtonsElement();
 	
-	var displaySetting = elevatedOptions.style.display;
-	if(displaySetting == "block") {
-		elevatedOptions.style.desplay = "none";
+	
+	
+	getStartTransactionElement().addEventListener("click", startTransactionClick);
+	getProductsElement().addEventListener("click", productsClick);
+	getCreateEmployeeElement().addEventListener("click", createEmployeeClick);
+	getSalesReportElement().addEventListener("click", salesReportClick);
+	getCashierReportElement().addEventListener("click", cashierReportClick);
+
+});
+
+
+function startTransactionClick(event) {
+		displayError("Functionality has not yet been implemented.");
+}
+
+function productsClick(event) {
+	ajaxDelete("/api/productListing", (callbackResponse) => {
+		if ((callbackResponse.data != null)
+			&& (callbackResponse.data.redirectUrl != null)
+			&& (callbackResponse.data.redirectUrl !== "")) {
+	
+			window.location.replace(callbackResponse.data.redirectUrl);
+		} else {
+			window.location.replace("/");
+		}
+	});
+}
+
+function createEmployeeClick(event) {
+	if(( ActiveUserEntity.classification == "Shift Manager" ) || ( ActiveUserEntity.classification == "General Manager" )) {
+		$("div.elevatedAccess").show();
 	}
-	
-	viewStartTransactionElement().addEventListener("click", displayError(errorMessage);
-	viewProductsElement().addEventListener("click", productsElementActionClickHandler);
-	viewCreateEmployeeElement().addEventListener("click", createEmployeeElementActionClickHandler);
-	viewSalesReportElement().addEventListener("click", displayError(errorMessage));
-	viewCashierReportElement().addEventListener("click", displayError(errorMessage));
-
-});	
-
-// TODO:: Adding proper routing tonight during 2-8
-function signOutActionClickHandler() {
-	ajaxDelete("/api/signOut", (callbackResponse) => {
+	ajaxDelete("/api/employee", (callbackResponse) => {
 		if ((callbackResponse.data != null)
 			&& (callbackResponse.data.redirectUrl != null)
 			&& (callbackResponse.data.redirectUrl !== "")) {
@@ -31,34 +47,48 @@ function signOutActionClickHandler() {
 			window.location.replace("/");
 		}
 	});
-	
-function signOutActionClickHandler() {
-	ajaxDelete("/api/signOut", (callbackResponse) => {
-		if ((callbackResponse.data != null)
-			&& (callbackResponse.data.redirectUrl != null)
-			&& (callbackResponse.data.redirectUrl !== "")) {
-	
-			window.location.replace(callbackResponse.data.redirectUrl);
-		} else {
-			window.location.replace("/");
-		}
-	});
-	
-function toggleButtonsElement() {
-	return document.getElementById("elevatedAccess";
 }
+
+function salesReportClick(event) {
+	if(( ActiveUserEntity.classification == "Shift Manager" ) || ( ActiveUserEntity.classification == "General Manager" )) {
+		$("div.elevatedAccess").show();
+	}
+		displayError("Functionality has not yet been implemented.");
+}
+
+function cashierReportClick(event) {
+	if(( ActiveUserEntity.classification == "Shift Manager" ) || ( ActiveUserEntity.classification == "General Manager" )) {
+		$("div.elevatedAccess").show();
+	}
+		displayError("Functionality has not yet been implemented.");
+}
+	
+function signOutActionClickHandler() {
+	
+	ajaxDelete("/api/signOut", (callbackResponse) => {
+		if ((callbackResponse.data != null)
+			&& (callbackResponse.data.redirectUrl != null)
+			&& (callbackResponse.data.redirectUrl !== "")) {
+	
+			window.location.replace(callbackResponse.data.redirectUrl);
+		} else {
+			window.location.replace("/");
+		}
+	});
+}
+
 function getStartTransactionElement() {
-	return document.getElementById("StartTransaction");
+	return document.getElementById("startTransaction");
 }
-function getViewProductsElement() {
-	return document.getElementById("ViewProducts");
+function getProductsElement() {
+	return document.getElementById("viewProducts");
 }
 function getCreateEmployeeElement() {
-	return document.getElementById("CreateEmployee");
+	return document.getElementById("createEmployee");
 }
 function getSalesReportElement() {
-	return document.getElementById("SalesReport");
+	return document.getElementById("salesReport");
 }
 function getCashierReportElement() {
-	return document.getElementById("CashierReport");
+	return document.getElementById("cashierReport");
 }
