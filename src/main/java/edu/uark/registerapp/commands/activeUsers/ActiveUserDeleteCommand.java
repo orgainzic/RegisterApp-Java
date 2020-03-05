550c83930c1aa444771a057ac4caac613007ce4a
Gunner L <zglawles@uark.edu>
2020-03-04 22:15:20 -0600

    Implemented ActiveUserDeleteCommand, untested

---
+++ src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
@@ -0,0 +1,37 @@
+package edu.uark.registerapp.commands.activeUsers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
+
+@Service
+public class ActiveUserDeleteCommand implements VoidCommandInterface {
+    @Override
+    public void execute(){
+        // Supposed to add functionality for validating Employee request object?
+        // Not sure where I'm supposed to find this employee object or why it matters
+
+        // Query and delete the active user
+        final Optional<ActiveUserEntity> queriedActiveUser =
+            this.activeUserRepository.findBySessionKey(this.currentSessionKey);
+        
+        if (queriedActiveUser.isPresent()){
+            this.activeUserRepository.delete(queriedActiveUser.get());
+        }
+        else{
+            throw new NotFoundException("ActiveUserEntity");
+        }
+    }
+
+    // Properties
+    private String currentSessionKey;
+
+    @Autowired
+    private ActiveUserRepository activeUserRepository;
+}
