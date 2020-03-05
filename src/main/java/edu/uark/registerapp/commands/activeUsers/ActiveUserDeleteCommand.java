3123a5ab204252b449d41ed365cc6a569918257c
TheFlyIsASpy <nicolassherid@gmail.com>
2020-03-05 14:27:51 -0600

    Big progress on signin 2

--- src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
+++ src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
@@ -1,13 +1,18 @@
 package edu.uark.registerapp.commands.activeUsers;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import edu.uark.registerapp.models.api.Employee;
 import edu.uark.registerapp.models.entities.ActiveUserEntity;
 import edu.uark.registerapp.commands.VoidCommandInterface;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
 import edu.uark.registerapp.models.repositories.ActiveUserRepository;
 
 @Service
@@ -16,8 +21,16 @@ public class ActiveUserDeleteCommand implements VoidCommandInterface {
     public void execute(){
         // Supposed to add functionality for validating Employee request object?
         // Not sure where I'm supposed to find this employee object or why it matters
+        if(StringUtils.isBlank(employee.getFirstName()) || StringUtils.isBlank(employee.getLastName())){
+            throw new UnprocessableEntityException("Name");
+        }
 
-        // Query and delete the active user
+        queryUserTable();  
+    }
+
+    @Transactional
+    public void queryUserTable(){
+    // Query and delete the active user
         final Optional<ActiveUserEntity> queriedActiveUser =
             this.activeUserRepository.findBySessionKey(this.currentSessionKey);
         
@@ -28,11 +41,15 @@ public class ActiveUserDeleteCommand implements VoidCommandInterface {
             throw new NotFoundException("ActiveUserEntity");
         }
     }
-    
     // Properties
     private String currentSessionKey;
     public void setCurrentSessionKey(String key){
         this.currentSessionKey = key;
+    }
+
+    private Employee employee;
+    public void setEmployee(Employee emp){
+        this.employee = emp;
     }
 
     @Autowired
