7d7ece7e5c6718ad8d3ce4ccb5301fe6ce662a31
Gunner L <zglawles@uark.edu>
2020-03-04 21:19:33 -0600

    Finished EmployeeSignInCommand, untested.

---
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -0,0 +1,104 @@
+package edu.uark.registerapp.commands.employees;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import javax.transaction.Transactional;
+import java.util.UUID;
+import org.apache.commons.lang3.StringUtils;
+
+import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.employees.helpers.EmployeeHelper;
+import edu.uark.registerapp.commands.exceptions.ConflictException;
+import edu.uark.registerapp.commands.exceptions.NotFoundException;
+import edu.uark.registerapp.commands.exceptions.UnprocessableEntityException;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
+
+@Service
+public class EmployeeSignInCommand implements VoidCommandInterface {
+    @Override
+    public void execute() {
+
+        this.validateSignInProperties();
+
+        this.validateSignInRequest();
+
+        this.queryActiveUser();
+
+
+    }
+
+    // Helper methods
+    private void validateSignInProperties(){
+        if (StringUtils.isBlank(this.employeeSignIn.getEmployeeId())){
+            throw new UnprocessableEntityException("employeeId");
+        }
+        else if (!(StringUtils.isNumeric(this.employeeSignIn.getEmployeeId()))){
+            throw new UnprocessableEntityException("employeeId");
+        }
+
+        if (StringUtils.isBlank(this.employeeSignIn.getPassword())){
+            throw new UnprocessableEntityException("password");
+        }
+    }
+    
+    private void validateSignInRequest(){
+        if (this.employeeRepository.existsByEmployeeId(Integer.parseInt(
+            EmployeeHelper.padEmployeeId(Integer.parseInt(
+                this.employeeSignIn.getEmployeeId()))))){
+            if (EmployeeHelper.hashPassword(this.employeeSignIn.getPassword()).equals(
+                this.employeeRepository.findByEmployeeId(Integer.parseInt(
+                    EmployeeHelper.padEmployeeId(Integer.parseInt(
+                        this.employeeSignIn.getEmployeeId())))).get().getPassword())){
+                            // do this if password matches
+                }
+            else {
+                throw new ConflictException("password");
+            }
+        }
+        else {
+            throw new NotFoundException("Employee");
+        }
+    }
+
+    @Transactional
+    private void queryActiveUser(){
+
+        final Optional<ActiveUserEntity> queriedActiveUser =
+            this.activeUserRepository.findByEmployeeId(
+                UUID.fromString(EmployeeHelper.padEmployeeId(
+                    Integer.parseInt(this.employeeSignIn.getEmployeeId()))));
+        
+        if (queriedActiveUser.isPresent()){
+            queriedActiveUser.get().setSessionKey(this.currentSessionKey);
+            this.activeUserRepository.save(queriedActiveUser.get());
+        }
+        else{
+            ActiveUserEntity newActiveUser = new ActiveUserEntity();
+            newActiveUser.setSessionKey(this.currentSessionKey);
+            newActiveUser.setEmployeeId(UUID.fromString(this.employeeSignIn.getEmployeeId()));
+            newActiveUser.setClassification(this.employeeRepository.findByEmployeeId(
+                Integer.parseInt(EmployeeHelper.padEmployeeId(Integer.parseInt(this.employeeSignIn.getEmployeeId())))
+                ).get().getClassification());
+            newActiveUser.setName(this.employeeRepository.findByEmployeeId(
+                Integer.parseInt(EmployeeHelper.padEmployeeId(Integer.parseInt(this.employeeSignIn.getEmployeeId())))
+                ).get().getLastName());
+            this.activeUserRepository.save(newActiveUser);
+        }
+        return;
+    }
+
+    // Properties
+    private EmployeeSignIn employeeSignIn;
+    private String currentSessionKey;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private ActiveUserRepository activeUserRepository;
+}
